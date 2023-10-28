6f09f1adcc1bf7e6761ecfb4c231bb935f11a028
Satabdi <satabdiraniweb@gmail.com>
2023-10-28 21:23:13 +0600

    Home added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { AiFillAccountBook } from 'react-icons/ai';
 
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+       {/* Container */}
+       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-left'>
+          <p className='text-pink-600'>Hi, my name is</p>
+          <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Satabdi Rani</h1>
+          <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Engineer (Frontend)</h2>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>I'am a fronend engineer I'am a fronend engineerI'am a fronend engineer I'am a fronend engineer I'am a fronend engineer I'am a fronend engineer</p>
+          <div>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work 
+            <span className='group-hover:rotate-90 duration-300'><AiFillAccountBook className='ml-3'></AiFillAccountBook></span> </button>
+          </div>
+       </div>
 
     </div>
   )
